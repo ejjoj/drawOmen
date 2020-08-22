2961b0dd1e28855b46969fb470927d6774db1f69
ejjoj <szymaskibartosz@gmail.com>
2020-08-22 19:43:42 +0200

    app built

--- app.js
+++ app.js
@@ -1,67 +1,91 @@
 const Button = (props) => {
   const {title, click} = props;
-
   return (
     <button onClick={click}>{title}</button>
   );
 }
 
-class Draw extends React.Component {
+class OmenDrawer extends React.Component {
   state = {
-    drawedID: 0,
-    text: '',
+    isClicked: false,
+    identifier: 0,
+    newOmenContent: '',
     omens: [
       {
         id: 1,
-        content: 'Pierwsza wróżba',
+        content: 'Pierwsza wróżba'
       },
       {
         id: 2,
-        content: 'Druga wróżba',
+        content: 'Druga wróżba'
       },
       {
         id: 3,
-        content: 'Trzecia wróżba',
+        content: 'Trzecia wróżba'
       },
     ]
   }
 
-  drawIndex = () => {
-    let maxIndex = 0;
-    if (this.state.omens) {
-      maxIndex = this.state.omens.length + 1;
-
-      this.setState({
-        drawedID: Math.floor((Math.random() * maxIndex))
-      });
+  handleDrawID = () => {
+    const max = this.state.omens.length;
+    const min = 0;
+    let flag = true;
+    let drawedNumber = 0;
+    while(flag) {
+      drawedNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      if (drawedNumber > 0) {
+        flag = false;
+      }
     }
-    this.drawOmen(this.state.drawedID);
+
+    this.setState({
+      identifier: drawedNumber,
+      isClicked: true
+    });
   }
 
-  drawOmen(id) {
-      if (id) {
-        const omen = this.state.omens.filter(omen => omen.id === id);
+  getRandomOmen(array, bool) {
+    if (bool) {
+      const newArray = array.filter(singleArray => singleArray.id === this.state.identifier);
+      alert(`Dostępne wróżby: ${array.map(singleArray => singleArray.content)}`);
+      return newArray.map(singleNewArray => singleNewArray.content);
+    } else return null;
+  }
 
-        this.setState({
-          text: omen.map(singleText => singleText.content)
-        });
-        alert(`Dostępne wróżby: ${this.state.omens.map(omen => omen.content)}`);
-      }
+  handleChange = e => {
+    this.setState({
+      newOmenContent: e.target.value,
+      isClicked: false
+    });
+  }
+
+  handleAddNewOmen = () => {
+    const text = this.state.newOmenContent;
+
+    if (text.length) {
+     let newID = this.state.omens[this.state.omens.length - 1].id + 1;
+     this.setState(prevState => ({
+       omens: [...prevState.omens, {id: newID, content: text}],
+       isClicked: false,
+       newOmenContent: ''
+     }));
+    }
   }
 
   render() {
-    const {text} = this.state;
+    const {omens, isClicked, newOmenContent} = this.state;
+    const text = this.getRandomOmen(omens, isClicked);
     return (
       <>
-        <Button title='Zobacz wróżbę' click={this.drawIndex}/>
+        <Button title='Losuj wróżbę' click={this.handleDrawID}/>
         <br />
-        <input type="text"/>
-        <Button title='Dodaj wróżbę'/>
+        <input value={newOmenContent} type="text" onChange={this.handleChange}/>
+        <Button title='Dodaj wróżbę' click={this.handleAddNewOmen}/>
         <br />
-        <h1>{text}</h1>
+        {text ? <h1>{text}</h1> : null}
       </>
     );
   }
 }
 
-ReactDOM.render(<Draw />, document.getElementById('root'));
+ReactDOM.render(<OmenDrawer />, document.getElementById('root'));
